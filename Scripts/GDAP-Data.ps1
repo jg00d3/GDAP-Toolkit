f4e139a559c991522febaebb35b2e542b40f1fc2
jg00d3 <sysadmroot@gmail.com>
2025-11-14 14:53:37 -0800

    Update GDAP-Data.ps1

--- Scripts/GDAP-Data.ps1
+++ Scripts/GDAP-Data.ps1
@@ -1,6 +1,6 @@
 <#
     GDAP-Data.ps1
-    Version: 1.0.9
+    Version: 1.0.10
 #>
 
 function Write-GdapLog {
@@ -18,26 +18,27 @@ function Write-GdapLog {
     "$timestamp [$Level] $Message" | Out-File -Append -FilePath $logFile
 }
 
-# -------------------------------------------------------
-# Retrieve base GDAP relationships
-# -------------------------------------------------------
+# ---------------------------------------------------------------------
+# Retrieve GDAP relationships (SAFE PAGING + separate accessDetails)
+# ---------------------------------------------------------------------
 function Get-GdapRelationships {
-    Write-GdapLog "Retrieving GDAP relationships…"
+    Write-GdapLog "Retrieving GDAP relationships..."
 
-    $url = "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships"
+    $items = @()
+    $url   = "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships"
 
-    $results = Invoke-MgGraphRequest -Method GET -Uri $url
-    $items = $results.value
-
-    while ($results.'@odata.nextLink') {
-        $results = Invoke-MgGraphRequest -Method GET -Uri $results.'@odata.nextLink'
-        $items += $results.value
+    do {
+        $resp = Invoke-MgGraphRequest -Method GET -Uri $url
+        if ($resp.value) { $items += $resp.value }
+        $url = $resp.'@odata.nextLink'
     }
+    while ($url)
 
-    # Retrieve accessDetails for each relationship
+    # Fetch accessDetails separately
     foreach ($rel in $items) {
-        $relId = $rel.id
-        $detailsUrl = "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$relId/accessDetails"
+
+        $id = $rel.id
+        $detailsUrl = "https://graph.microsoft.com/beta/tenantRelationships/delegatedAdminRelationships/$id/accessDetails"
 
         try {
             $rel.accessDetails = Invoke-MgGraphRequest -Method GET -Uri $detailsUrl
@@ -52,27 +53,25 @@ function Get-GdapRelationships {
     return $items
 }
 
-# -------------------------------------------------------
-# Role Definitions Map
-# -------------------------------------------------------
+# ---------------------------------------------------------------------
+# Role Definitions
+# ---------------------------------------------------------------------
 function Get-GdapRoleDefinitionsMap {
-    Write-GdapLog "Retrieving role definitions…"
+
+    Write-GdapLog "Retrieving role definitions..."
 
     $url = "https://graph.microsoft.com/beta/roleManagement/directory/roleDefinitions?`$select=id,displayName"
-
-    $results = Invoke-MgGraphRequest -Method GET -Uri $url
+    $resp = Invoke-MgGraphRequest -Method GET -Uri $url
 
     $map = @{}
-    foreach ($role in $results.value) {
-        $map[$role.id] = $role.displayName
-    }
+    foreach ($role in $resp.value) { $map[$role.id] = $role.displayName }
 
     return $map
 }
 
-# -------------------------------------------------------
+# ---------------------------------------------------------------------
 # Unified Assignments Table
-# -------------------------------------------------------
+# ---------------------------------------------------------------------
 function Get-GdapAccessAssignments {
     param(
         [array]$Relationships,
@@ -83,23 +82,17 @@ function Get-GdapAccessAssignments {
 
     foreach ($rel in $Relationships) {
 
-        $customerId     = $rel.customerId
-        $relationshipId = $rel.id
-        $displayName    = $rel.displayName
-
         $details = $rel.accessDetails
         if (-not $details) { continue }
 
-        $start = $details.startDateTime
-        $end   = $details.endDateTime
-
         foreach ($roleId in $details.unifiedRoles) {
+
             $output += [pscustomobject]@{
-                CustomerId     = $customerId
-                RelationshipId = $relationshipId
-                DisplayName    = $displayName
-                StartDateTime  = $start
-                EndDateTime    = $end
+                CustomerId     = $rel.customerId
+                RelationshipId = $rel.id
+                DisplayName    = $rel.displayName
+                StartDateTime  = $details.startDateTime
+                EndDateTime    = $details.endDateTime
                 RoleId         = $roleId
                 RoleName       = $RoleMap[$roleId]
             }
